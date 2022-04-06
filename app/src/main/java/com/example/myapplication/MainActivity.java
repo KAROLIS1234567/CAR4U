77848d58dcc98d33744ab97f9b9547ee4f286787
Lukas <23.lukas.mecionis@azuolynogimnazija.lt>
2022-04-06 22:15:42 +0300

    CAR4U

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,11 +8,23 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+private Button button;
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_MyApplication);
         setContentView(R.layout.activity_main);
 
-}}
+        button = (Button) findViewById(R.id.button);
+button.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        openActivity2();
+    }
+});
+}
+public void openActivity2(){
+    Intent intent = new Intent(this, Activity2.class);
+    startActivity(intent);
+}
+}
