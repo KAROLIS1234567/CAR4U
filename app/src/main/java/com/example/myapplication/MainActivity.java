e7ee003a332106576256275ab853e7f7dde3b248
Lukas <23.lukas.mecionis@azuolynogimnazija.lt>
2022-04-10 11:02:21 +0300

    Lukas

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -27,4 +27,20 @@ public void openActivity2(){
     Intent intent = new Intent(this, Activity2.class);
     startActivity(intent);
 }
+
+
+    public void radioButtonhandler(View view) {
+
+        // Decide what happens when a user clicks on a button
+    }
+
+
+
+
+
+
+    public void submitbuttonHandler(View view) {
+        //Decide what happens when the user clicks the submit button
+
+    }
 }
